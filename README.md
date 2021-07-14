600d5a9549ca3dbc18d0843a488d547448cc025b
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-07-14 15:57:54 -0400

    redshift init

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 ## Project Overview
 
+This project is an ETL using Spotipy and general a weekly email of all the songs played and web scraper additionnal information for each artist listenned.
+
+This program uses Spotipy, Regex, Apache Airflow and AWS Redshift.
+
+After the extraction, I web scrape additionnal information like birthday of the artist and transform the data using python to clean it up, create unique identifiers, and load it into a Redshift database.
+
+The loading step uses (1) SQL to query the data (2) and python to automate a weekly email that gets sent to my email giving a summary of my Spotify listening for that week.
+
+### Preview
+
 ![preview](airflow_init.png)
 
 > python3 -m venv env
--- api/dags/spotify_wiki_scraper_job.py
+++ api/dags/spotify_wiki_scraper_job.py
@@ -3,15 +3,20 @@ import json
 import string
 import requests
 from bs4 import BeautifulSoup
-
+import re
+import datetime
 
 def get_track_details(artist = "2Pac", track = "California Love"):
     # Get year
     try:
         r = requests.get('https://musicbrainz.org/search?query={}+{}&type=release&method=indexed'.format(artist, track).replace(' ', '+'))
         soup = BeautifulSoup(r.content, 'lxml')
-        year = soup.find_all('span', 'release-date')[0] # Select first result
+        yearHTML = soup.find_all('span', 'release-date')[0] # Select first result
+
+        year = re.findall(r'[0-9]+', str(yearHTML))
+
         print(year)
+
     except AttributeError:
         year = 'Year Not Found'
 
@@ -20,8 +25,9 @@ def get_track_details(artist = "2Pac", track = "California Love"):
         w = requests.get('https://en.wikipedia.org/wiki/{}'.format(artist).replace(' ', '_'))
         soupW = BeautifulSoup(w.content, 'lxml')
 
-        bday = soupW.find_all('span', class_='bday')
-        print(bday)
+        bdayHTML = soupW.find_all('span', class_='bday')
+        year = re.findall(r'(\d{4})[-](\d{2})[-](\d{2})', str(bdayHTML))
+        print(year)
 
         return 1
     except:
